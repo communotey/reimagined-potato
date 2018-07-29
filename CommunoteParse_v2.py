20a385f6cccf9681d74be7ee278c51a767bc9fd0
Kemal Ahmed <goatandsheep@gmail.com>
2018-07-28 20:47:36 -0400

    started to include actual syntax

--- CommunoteParse_v2.py
+++ CommunoteParse_v2.py
@@ -10,67 +10,64 @@ def main():
 
 
     for line in content:
-        lineList = re.split("\t", line)
-        index = lineList[0]
-        facilty = lineList[1]
-        courseCode = lineList[3]
-        originalTitle = lineList[4]
-        fileURL = lineList[6]
-        
-        typeTags = getMatType(originalTitle)
-        year = getYear(originalTitle)
-        isPDF = checkIfPDF(originalTitle)
+        data = parseInput(line)
+        if (data.format is not None):
 
-        #Just printing the files
-        if (isPDF):
-            print str(typeTags) + "\t"*(3-len(typeTags)) + year + "\t" + originalTitle
+    #Just printing the files
+            print str(typeTags) + "\t"*(3-len(typeTags)) + data.dateCreated + "\t" + originalTitle
             
         
-def checkIfPDF(originalTitle):
-    if ((re.search('pdf?$', originalTitle, re.IGNORECASE)) is not None):
-        return True
+def getFormat(filename):
+    sections = re.split('.')
+    extension = sections[len(sections) - 1]
+
+    if (extension is not None):
+        return extension
     else:
         return False
 
-def getMatType(originalTitle):
-    allTypeTags = []
+def getMatType(filename):
+    words = filename.split(' ')
     #FindAll to cover straight forward types that have consistent spelling
     typeTagsRaw = re.findall('quiz|tutorial|assignment', originalTitle, re.IGNORECASE)
     for tag in typeTagsRaw:
-        allTypeTags.append(tag.upper())
+        return tag.lower()
 
     #Tags as EXAM. FindAll exam would find 'example' and marks as EXAM
     examTag = re.search('exam', originalTitle, re.IGNORECASE)
     if (examTag is not None):
-        allTypeTags.append("EXAM")
+        return "exam"
 
     #Tags as TEST. #Has cases where tags midterm practice questions as TEST
     testTag = re.search('test|midterm', originalTitle, re.IGNORECASE)
     if (testTag is not None):
-        allTypeTags.append("TEST")
+        return "test"
     
     #Tags as a NOTE
     noteTag = re.search('note|lecture|review', originalTitle, re.IGNORECASE)
     if (noteTag is not None):
-        allTypeTags.append("NOTE")
+        return "note"
 
     #Tags as NOTE if contains 'chapter' but isn't already tagged as a QUIZ
     chapterNoteTag = re.search('chapter|chap', originalTitle, re.IGNORECASE)
     if ((chapterNoteTag is not None) and not('QUIZ' in allTypeTags) and not('NOTE' in allTypeTags)):
-        allTypeTags.append("NOTE")
+        return "note"
 
     #Tags as a CHEATSHEET
     noteTag = re.search('cheat|crib|summary|formula', originalTitle, re.IGNORECASE)
     if (noteTag is not None):
-        allTypeTags.append("NOTE")
+        return "cheatsheet"
     
-    
-    #Tags as a SOLUTION
+    # default is note
+    return "note"
+
+def getSol(filename):
+    # Tags as a SOLUTION
     solutionTag = re.search('solution|soln|answer', originalTitle, re.IGNORECASE)
     if (solutionTag is not None):
-        allTypeTags.append("SOLUTION")
-        
-    return allTypeTags
+        return true
+    else:
+        return false
 
 
 def getYear(originalTitle):
@@ -79,6 +76,37 @@ def getYear(originalTitle):
         return yearSearch.group()
     else:
         return ""
+
+def getDriveMetaData():
+    return {}
+
+def uploadS3():
+    print 'boom'
+
+def parseInput(input):
+    
+    output = {}
+
+    lineList = re.split("\t", input)
+    index = lineList[0]
+
+    output['facilty'] = lineList[1]
+    output['code'] = lineList[3]
+
+    # TODO: get format from Google Drive API
+    # output['format'] = getFormat(lineList[4])
+    output['format'] = 'link'
+    text = lineList[4].split('.')
+    if (len(text)>1):
+        text.pop()
+    output['description'] = " ".join(text)
+    words = text.split(' ')
+    output['S3OrginalLink'] = lineList[6]
+    
+    typeTags = getMatType(originalTitle)
+    year = getYear(originalTitle)
+
+    return output
 
 main()
     
