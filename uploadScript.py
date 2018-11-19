64182b4b281173a7773895177fad9b17599e48f5
David Cleave <cleave.david@gmail.com>
2018-11-18 21:16:46 -0500

    Updated upload script
    Was working but got 500 errors again. Added requests to create a new course if it doesn't exist already.

--- uploadScript.py
+++ uploadScript.py
@@ -9,6 +9,10 @@ import requests
 #TODO: Scrape semester, eval number etc in parsing script better.
 
 def main():
+    #TODO: read userId and jwt from secure file
+    userId = ""
+    jwt = ""
+
     #open output.txt
     #f = open("output.txt")
     f = open("outputShort.txt")
@@ -20,56 +24,88 @@ def main():
     for line in content:
         data = json.loads(line)
         if data["format"] in validFormats:
+            #make a new course if it doesn't exist
+
+            reqCourseUrl = "https://api.communote.net/api/course?urlId=MCMASTER-UNIVERSITY-HAM-ONT-CAN&code=" + data["code"].replace(" ", "-").lower()
+            print reqCourseUrl
+            r = requests.get(reqCourseUrl)
+
+             # the problem is you get a 200 then redirects to a error page. Look into how to determine if the course exists or not.
+            if r.status_code == 404:
+                print "status code for course: " + str(r.status_code)
+                newCourse(data["code"], userId, jwt)
+            else:
+                print "course exists"
             #call upload(data)
-            upload(data)
+            upload(data, userId, jwt)
         else:
             print "Skipping upload. Format: " + data["format"]
 
-def upload(data):   
+
+def newCourse(code, userId, jwt):
+    reqUrl = "https://api.communote.net/api/course/new"
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    data = {"contents":{"group":"","name":code.replace("-", " "),"title":code.replace("-", " ")},"jwt":jwt"schoolUrl":"MCMASTER-UNIVERSITY-HAM-ONT-CAN","userId":userId}
+
+
+    r = requests.post(reqUrl, data=json.dumps(data), headers=headers)
+
+    if r.status_code == 200:
+        print "Course: " + code + " created."
+
+    else:
+        print "Error creating course: " + code + " Status code: " + str(r.status_code)
+
+    return
+
+
+
+def upload(file_data, userId, jwt):   
     #send request to communote api with data 
     reqURL = "https://api.communote.net/api/file/new"
     #reqURL = "http://httpbin.org/post"
 
     #adjust the format to what it has been downloaded as
-    if data["format"] == "gdoc" or data["format"][0:3] == "doc":
+    if file_data["format"] == "gdoc" or file_data["format"][0:3] == "doc":
         file_format = "odt"
     else:
-        file_format = data["format"]
+        file_format = file_data["format"]
 
-    dateCreated = data["year"] #need to adjust this to be a date, not just year
-    description = data["description"]
-    semesterID = data["semesterId"]
-    solution = data["solution"]
-    file_type = data["format"] #what is type??? there is also a format param
-    Version = data["version"] #TODO: Add versions to parser, look for single digit
-    Volume = data["volume"] #add to the parser. i.e. Test 1 vs. Test 2
-    S3OriginalLink = "https://docs.google.com/open?id=" + data["fileID"]
-    _creator = "5b3699ba791da980bbfeafea" #my user ID? what name to I upload it under?
-    _course = data["code"]
-    _school = "MCMASTER-UNIVERSITY-HAM-ONT-CAN" #is this correct?
+    #dateCreated = data["year"] #need to adjust this to be a date, not just year
+    dateCreated = "2018-11-18T03:14:22.884Z"
+    description = file_data["description"]
+    semesterId = file_data["semesterId"]
+    solution = file_data["solution"]
+    file_type = file_data["format"] #what is type??? there is also a format param
+    Version = file_data["version"] #TODO: Add versions to parser, look for single digit
+    Volume = file_data["volume"] #add to the parser. i.e. Test 1 vs. Test 2
+    S3OriginalLink = "https://docs.google.com/open?id=" + file_data["fileID"]
+    courseUrl = file_data["code"]
+    schoolUrl = "MCMASTER-UNIVERSITY-HAM-ONT-CAN" #is this correct?
     #TODO: How will the actual file attached if using a POST request
 
     #/downloads/MATH 1ZA3/Test 2.pdf
-    filePath = 'downloads/' + _course + '/' + description + '.' + file_format
-    file = {'file':open(filePath, 'rb')}
+    fileName = description + '.' + file_format
+    filePath = 'downloads/' + courseUrl + '/'
+    file = {'file-to-upload':open(filePath + fileName, 'rb')}
 
- 
+ #
     data = {"description": description,
             "dateCreated": dateCreated,
-            "semesterID": semesterID,
+            "semesterId": semesterId,
             "solution": solution,
             "type": file_type,
             "Version": Version,
             "Volume": Volume,
-            "S3OriginalLink": S3OriginalLink,
-            "_creator": _creator,
-            "_course": _course,
-            "_school": _school
-            }
+            "userId": userId,
+            "courseUrl": courseUrl.replace(" ", "-").lower(),
+            "schoolUrl": schoolUrl,
+            "jwt": jwt
+            } 
     
 
-    print "data: "
-    print data
     r = requests.post(reqURL, data=data, files=file)
 
     print
@@ -79,8 +115,10 @@ def upload(data):
     print "request headers: "
     print r.request.headers
     print
-    #print "request body: "
-    #print r.request.body
+    print "request body: "
+    print r.request.body[:2000]
+    print 
+    print "==========================================================================================================================="
     print 
 
 
